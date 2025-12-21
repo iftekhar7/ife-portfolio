89fbd0d86a6eb415235a1be53175c8bd7dbb1caa
Mohammad Iftekhar <mohammad.iftekhar@iot83.com>
2025-12-21 23:30:55 +0530

    project image added

--- src/pages/Homes/Contact/index.tsx
+++ src/pages/Homes/Contact/index.tsx
@@ -85,7 +85,7 @@ function Contact() {
           </div>
           <div className="card-body pb-0 ">
             <div className="grid-basic">
-              <div className="">
+              <div className="pr-4">
                 <h6 className="text-sub-heading mb-1">We Are Here For You</h6>
                 <p className="text-sm mb-6">
                   For more info on how we can deliver your project, and anything
@@ -125,88 +125,102 @@ function Contact() {
                   </div>
                 </div>
                 <h6 className="text-sub-heading">Follow Us</h6>
-                <div className="social-links mt-0 mb-12">
-                  {socialIconData.map((item) => (
-                    <button
-                      key={item.id}
-                      onClick={() => handleLinkNavigate(item.sourceLink)}
-                      className={`btn btn-sm btn-${item.className}`}
-                    >
-                      <i className={item.icon}></i>
-                    </button>
-                  ))}
+                <div className="social-links mt-0">
+                  {socialIconData.map((item) =>
+                    item.isTrue ? (
+                      <button
+                        key={item.id}
+                        onClick={() => handleLinkNavigate(item.sourceLink)}
+                        className={`btn btn-sm btn-${item.className}`}
+                      >
+                        <i className={item.icon}></i>
+                      </button>
+                    ) : null
+                  )}
                 </div>
               </div>
               <MapSection />
             </div>
-            <form ref={form} onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label className="form-label">Your Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter your good name?"
-                  name="from_name"
-                  value={name}
-                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                    setName(e.target.value)
-                  }
-                />
-                {errors.name && (
-                  <span className="error">
-                    <i className="fas fa-exclamation-circle"></i> {errors.name}
-                  </span>
-                )}
-              </div>
-              <div className="form-group">
-                <label className="form-label">Your Email</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter your Email Address"
-                  name="from_email"
-                  value={email}
-                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                    setEmail(e.target.value)
-                  }
-                />
-                {errors.email && (
-                  <span className="error">
-                    <i className="fas fa-exclamation-circle"></i> {errors.email}
-                  </span>
-                )}
-              </div>
-              <div className="form-group">
-                <label className="form-label">Your Message</label>
-                <textarea
-                  rows={7}
-                  className="form-control"
-                  placeholder="What you want to say?"
-                  name="message"
-                  value={message}
-                  onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
-                    setMessage(e.target.value)
-                  }
-                />
-              </div>
-              {/* <ReCAPTCHA
+            <div className="contact-form mt-12">
+              <h4 className="text-heading">We Want To Here From You</h4>
+              <form ref={form} onSubmit={handleSubmit}>
+                <div className="flex">
+                  <div className="flex-50 pr-2">
+                    <div className="form-group">
+                      <label className="form-label">Your Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter your good name?"
+                        name="from_name"
+                        value={name}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                          setName(e.target.value)
+                        }
+                      />
+                      {errors.name && (
+                        <span className="error">
+                          <i className="fas fa-exclamation-circle"></i>{" "}
+                          {errors.name}
+                        </span>
+                      )}
+                    </div>
+                  </div>
+                    <div className="flex-50 pl-2">
+                      <div className="form-group">
+                        <label className="form-label">Your Email</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Enter your Email Address"
+                          name="from_email"
+                          value={email}
+                          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setEmail(e.target.value)
+                          }
+                        />
+                        {errors.email && (
+                          <span className="error">
+                            <i className="fas fa-exclamation-circle"></i>{" "}
+                            {errors.email}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                </div>
+
+                <div className="form-group">
+                  <label className="form-label">Your Message</label>
+                  <textarea
+                    rows={7}
+                    className="form-control"
+                    placeholder="What you want to say?"
+                    name="message"
+                    value={message}
+                    onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+                      setMessage(e.target.value)
+                    }
+                  />
+                </div>
+                {/* <ReCAPTCHA
                   sitekey="6Ldr4V4pAAAAAC9zhtuGpSCzzIg4_kIbEblt6rBB"
                   onChange={recaptchaHandler}
                   theme="dark"
                 /> */}
-              <div className="card-footer">
-                <div className="flex-end w-100">
-                  <button
-                    type="submit"
-                    value="Send"
-                    className="btn btn-primary"
-                    // disabled={!verified}
-                  >
-                    Send
-                  </button>
+                <div className="card-footer">
+                  <div className="flex-end w-100">
+                    <button
+                      type="submit"
+                      value="Send"
+                      className="btn btn-primary"
+                      // disabled={!verified}
+                    >
+                      Send
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </>
