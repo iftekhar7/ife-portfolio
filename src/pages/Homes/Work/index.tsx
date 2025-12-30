edd3d31a92ae9721e05d8ad71fef108a41db2a01
Mohammad Iftekhar <mohammad.iftekhar@iot83.com>
2025-12-31 01:27:27 +0530

    experience added

--- src/pages/Homes/Work/index.tsx
+++ src/pages/Homes/Work/index.tsx
@@ -3,36 +3,30 @@ import { data } from "./data";
 function Work() {
   return (
     <div className="timeline-section">
-      <div className="timeline-container">
-        <div className="timeline-line" />
-
-        <div className="timeline-items">
-          {data.map((item) => (
-            <div key={item.id} className="timeline-item">
-              <div className={`timeline-dot ${item.gradientClass}`}>
-                <div className="dot-inner" />
+      <div className="timeline-line" /> 
+      {data.map((item) => (
+        <div key={item.id} className="timeline-item">
+          <div className={`timeline-dot ${item.gradientClass}`}>
+            <div className="dot-inner" />
+          </div> 
+          <div className="timeline-card card">
+            <div className="card-header flex-between flex-align-start">
+              <div>
+                <h4 className="text-heading mb-1">{item.role}</h4>
+                <p className="text-primary">{item.company}</p>
               </div>
-
-              <div className="timeline-card card">
-                <div className="card-header flex-between">
-                <h4 className="role-title">{item.role}</h4>
-                <div className="pill pill-secondary mr-0">{item.year}</div> 
-                </div>
-
-                <p className="company-name light">{item.company}</p>
-
-                <ul className="pl-8">
-                  {item.description.map((subItem, index) => (
-                    <li className="text-sm" key={`${item.id}-${index}`}>
-                      {subItem.paragraph}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ))}
+              <span className="pill pill-secondary mr-0">{item.year}</span>
+            </div> 
+            <ul className="pl-8">
+              {item.description.map((subItem, index) => (
+                <li className="text-sm mb-2" key={`${item.id}-${index}`}>
+                  {subItem.paragraph}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
