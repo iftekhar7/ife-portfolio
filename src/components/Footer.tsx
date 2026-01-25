0a4ad8d030fb5a5ade8283c5719f83d0dcf714b3
Mohammad Iftekhar <mohammad.iftekhar@iot83.com>
2026-01-26 00:43:19 +0530

    git souce code added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,20 @@ function Footer() {
   return (
     <footer className={`py-12 px-6 border-top`}>
       <div className="max-w-7xl mx-auto text-center">
-        <p className={`mb-4  `}>Let's create something amazing together</p>
-        <p className={`text-sm `}>
+        <p>Let's create something amazing together</p>
+        <p className={`text-sm my-3`}>
           © 2026 Mohammad Iftekhar • Frontend Software Engineer • Gurugram,
           India
+        </p>
+        <p>
+          <a
+            href="https://github.com/iftekhar7/ife-portfolio.git"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-hover-underlined"
+          >
+          <i className="fab fa-github mr-1"/>  View Source on GitHub
+          </a>
         </p>
       </div>
     </footer>
